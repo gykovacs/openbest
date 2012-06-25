02e79a1f36ff4b193255b6b2df45c0d17172dd12
Gyorgy Kovacs <gyuriofkovacs@gmail.com>
2012-06-25 10:34:01 +0200

    build environment created

---
+++ src/lib/ds/hello-openbest.c
@@ -0,0 +1,6 @@
+#include <ds/hello-openbest.h>
+
+void helloOpenBEST()
+{
+    printf("Hello Open BEST build environment");
+}
---
+++ src/test/test-ds/test-ds.c
@@ -0,0 +1,8 @@
+#include <ds/hello-openbest.h>
+
+int main(int argc, char** argv)
+{
+    helloOpenBEST();
+    
+    return 0;
+}
